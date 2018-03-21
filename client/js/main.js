4d4025fa0cd8dcc341f960b338f45589d3f9d7e5
andreykuleb <andreykuleb@mail.ru>
2018-03-21 11:52:11 +0300

    find close border for best angle and template is work

--- client/js/main.js
+++ client/js/main.js
@@ -6,25 +6,54 @@ const angleStep = 10;
     result -1 - внутри границы;
     result >=0 - вне границы;
 */
+// function minBorderDistance(pixel, template){
+//     return template.reduce((distance, section)=>{
+//         //если пиксель уже внутри границ, то продолжаем возвращать -1
+//         if (distance == 0) return 0;
+//         //если пиксель стал внутри границ, начинаем возвращать -1
+//         if ((section.beg < pixel.h) && (section.end > pixel.h)){
+//             return 0;
+//         }
+//         if ((section.beg < (pixel.h + 360)) && (section.end > (pixel.h + 360))){
+//             return 0;
+//         }
+//         //иначе возвращаем минимальное значение между границами и предыдущим вычисленным значением
+//         return Math.min(Math.abs(pixel.h - section.beg),
+//                         Math.abs(pixel.h - section.end),
+//                         Math.abs((pixel.h + 360) - section.beg),
+//                         Math.abs((pixel.h + 360) - section.end),
+//                         distance
+//                      );
+//     }, 1000);
+// }
+function distanceMin(){
+    var minDistance = {"distance": 1000, "border": 0};
+    for (var i = 0; i < arguments.length; i++) {
+        if (minDistance.distance > arguments[i].distance) {
+            minDistance = arguments[i];
+        };
+      }
+    return minDistance;
+}
+
 function minBorderDistance(pixel, template){
-    return template.reduce((distance, section)=>{
+    return template.reduce((inf, section)=>{
         //если пиксель уже внутри границ, то продолжаем возвращать -1
-        if (distance == 0) return 0;
+        if (inf.distance == 0) return {"distance": 0, "border": 0};
         //если пиксель стал внутри границ, начинаем возвращать -1
         if ((section.beg < pixel.h) && (section.end > pixel.h)){
-            return 0;
+            return {"distance": 0, "border": 0};
         }
         if ((section.beg < (pixel.h + 360)) && (section.end > (pixel.h + 360))){
-            return 0;
+            return {"distance": 0, "border": 0};
         }
         //иначе возвращаем минимальное значение между границами и предыдущим вычисленным значением
-        return Math.min(Math.abs(pixel.h - section.beg),
-                        Math.abs(pixel.h - section.end),
-                        Math.abs((pixel.h + 360) - section.beg),
-                        Math.abs((pixel.h + 360) - section.end),
-                        distance
-                     );
-    }, 1000);
+        return distanceMin({"distance": Math.abs(pixel.h - section.beg), "border": section.beg},
+                            {"distance": Math.abs(pixel.h - section.end), "border": section.end},
+                            {"distance": Math.abs((pixel.h + 360) - section.beg), "border": section.beg},
+                            {"distance": Math.abs((pixel.h + 360) - section.end), "border": section.end},
+                            {"distance": inf.distance, "border": 0});
+    }, {"distance": 1000, "border": 0})
 }
 
 window.onload = function(){
@@ -61,13 +90,13 @@ window.onload = function(){
                 return {"beg": value.beg+angle, "end": value.end+angle};
             })
             //считаем сумму растояний пикслей до ближайщей границы нового шаблона
-            console.log(dataHSL[0]);
+            //console.log(dataHSL[0]);
+
 
             var sum = dataHSL.reduce((sum, pixel) => {
                 //console.log(sum);
-                return sum + minBorderDistance(pixel, template)
+                return sum + minBorderDistance(pixel, template).distance;
             }, 0);
-
             // dataHSL.forEach((pixel) => {
             //     //console.log(sum);
             //     console.log(minBorderDistance(pixel, template));
@@ -82,6 +111,22 @@ window.onload = function(){
             }
             minSumDistance = Math.min(minSumDistance, sum);
         }
+
+        template = template1.map((value) => {
+            return {"beg": value.beg+bestAngle, "end": value.end+bestAngle};
+        })
+
+        dataHSL = dataHSL.map((pixel) => {
+            //console.log(minBorderDistance(pixel, template).distance);
+            var data = minBorderDistance(pixel, template);
+            pixel.distance = data.distance; 
+            pixel.border = data.border;
+            return pixel;
+        }, 0);
+
+        dataHSL.forEach((i) => {
+            console.log(i.border);
+        });
 
         console.log(bestAngle);
         //console.log(minBorderDistance({ "h": 50}, template1));
